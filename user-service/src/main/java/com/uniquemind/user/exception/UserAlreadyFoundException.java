75dc37d43de5ffd8c68a9cdb08dddc2f9837cf93
greentree <greentree275@gmail.com>
2019-03-24 23:26:40 +1100

    Serial ID

--- user-service/src/main/java/com/uniquemind/user/exception/UserAlreadyFoundException.java
+++ user-service/src/main/java/com/uniquemind/user/exception/UserAlreadyFoundException.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Nag
  *
  */
-@SuppressWarnings("serial")
 @ResponseStatus(code=HttpStatus.BAD_REQUEST, reason="User ALready Exists")
 public class UserAlreadyFoundException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	
 
 }
--- user-service/src/main/java/com/uniquemind/user/exception/UserNotFoundException.java
+++ user-service/src/main/java/com/uniquemind/user/exception/UserNotFoundException.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Nag	
  *
  */
-@SuppressWarnings("serial")
 @ResponseStatus(code=HttpStatus.NOT_FOUND, reason = "User not found")
 public class UserNotFoundException extends Exception{
 
+	private static final long serialVersionUID = 1L;
+
+	
 }
